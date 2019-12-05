ff19147a4ad65e3cb993530ec2317915a92b1732
Waylon Dixon <wdixon@waylondixon.com>
2019-12-05 13:58:24 -0700

    npm server: added code for responding to post

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ serverApp.get('/', function(req, res){
   res.send('Hello from server');
 });
 
+serverApp.post('/register', function(req, res){
+  console.log(req.body);
+  res.status(200).send({"message": "Data received"});
+});
+
 serverApp.listen(PORT, function(){
   console.log("Server running on localhost:"+PORT);
 });
